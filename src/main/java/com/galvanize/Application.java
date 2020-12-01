56329e3f121cf97308c050e2a04bb979d2f25fe3
manzaloros <zacharymansellcello@gmail.com>
2020-12-01 13:32:23 -0600

    Add initial files

---
+++ src/main/java/com/galvanize/Application.java
@@ -0,0 +1,7 @@
+package com.galvanize;
+// Hello World app
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("hello world from Java app");
+    }
+}
