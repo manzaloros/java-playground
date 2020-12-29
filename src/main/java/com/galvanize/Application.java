b88b0fb2e06a46b3f548563ede3d9d6563de5ae8
zacharymansell <zach.mansell@galvanize.com>
2020-12-29 13:35:31 -0600

    Commit files to check ssh keys on removal of config file

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -1,29 +1,7 @@
 package com.galvanize;
 
-import java.util.ArrayList;
-
-// Hello World app
 public class Application {
-    private static void passVariable(ArrayList<String> x) {
-        x.add("item from inside method");
-        System.out.println("Array List inside method: " + x);
-    }
-
-
     public static void main(String[] args) {
-        
-        class Person {
 
-        }
-        Person person = new Person();
-        // Have to instantiate in order to call method, if NOT static
-        // Opposite of static? Instance, or non-static
-
-        ArrayList<String> var = new ArrayList<String>();
-        passVariable(var);
-        var.add("Item from outer scope");
-        Object o = new Object();
-
-        System.out.println(o instanceof Person);
     }
 }
